c647dd5c665b5a141b1b7d41377fe1398bfc9854
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-08-08 15:11:19 -0300

    fix: Fix project name

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Barbar Shop",
+  title: "Barber Shop",
   description: "Barber shop, the place where you'll get a new cut and become a new person",
 };
 
