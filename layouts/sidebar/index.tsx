90763927b3bbbd3aeb12567d2b538e7371930f71
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-08-16 01:17:40 -0300

    style: add animation effect in the navigation links

--- layouts/sidebar/index.tsx
+++ layouts/sidebar/index.tsx
@@ -12,22 +12,24 @@ export const Sidebar = ({
   return (
     <>
       <div
-      data-testid="sidebar"
+        data-testid="sidebar"
         className={`sidebar flex flex-col ${
           !isSidebarOpen ? "-translate-x-full" : "translate-x-0"
         } transition-transform duration-300 ease-in-out w-[220px] fixed top-0 left-0 items-center min-h-svh z-20 pt-2 bg-black md:hidden`}
       >
-        <ul className="flex flex-col gap-3 mt-2">
-          <Brand  testId="sidebar-brand"/>
-          {navigationLinks.map((link: any) => (
-            <li key={link.name} className="w-full ">
-              <NavigationLink
-                key={link.name}
-                route={link.route}
-                name={link.name}
-              />
-            </li>
-          ))}
+        <ul className="flex flex-col gap-6 mt-5">
+          <Brand testId="sidebar-brand" />
+          <div>
+            {navigationLinks.map((link: any) => (
+              <li key={link.name} className="w-20">
+                <NavigationLink
+                  key={link.name}
+                  route={link.route}
+                  name={link.name}
+                />
+              </li>
+            ))}
+          </div>
         </ul>
       </div>
       {isSidebarOpen && <Overlay data-testid="overlay" />}
