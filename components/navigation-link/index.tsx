44fa17996319044b55145ad060e15d94060cfc17
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-08-16 01:19:26 -0300

    style: update color schemes

--- components/navigation-link/index.tsx
+++ components/navigation-link/index.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-
+import "./styles.css";
 export const NavigationLink = ({
   route,
   name,
@@ -11,9 +11,10 @@ export const NavigationLink = ({
     <Link
       href={route}
       title={name}
-      className="text-white transition duration-300 ease-in-out hover:text-yellow-400"
+      className="nav-link text-white text-lg font-bold transition duration-300 p-1 ease-in-out "
     >
       {name}
+      <div className="bg-yellow-400"></div>
     </Link>
   );
 };
