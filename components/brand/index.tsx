a5f5429a778f0a26fdd1d18c13ca963205277b4a
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-08-16 01:20:16 -0300

    style: update brand colors and responsivess

--- components/brand/index.tsx
+++ components/brand/index.tsx
@@ -1,8 +1,13 @@
 import Link from "next/link";
 
-export const Brand = ({testId}: {testId: string}) => {
+export const Brand = ({ testId }: { testId: string }) => {
   return (
-    <Link data-testid={testId} href="/" title="Logo" className="text-yellow-400 text-xl font-bold">
+    <Link
+      data-testid={testId}
+      href="/"
+      title="Logo"
+      className="text-yellow-400 text-3xl font-bold md:text-4xl"
+    >
       BarberXYZ
     </Link>
   );
